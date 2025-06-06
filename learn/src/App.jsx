8642ffc5168fa2b700ac3528599ee1725e67e6b2
meghal limba <limbameghal8@gmail.com>
2025-06-06 19:49:28 +0530

    dummy website

---
+++ learn/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ResponsiveAppBar from './component/ResponsiveAppBar'
+
+import MediaCover from './component/MediaCover'
+
+import ColorInversionFooter from './component/ColorInversionFooter'
+const App = () => {
+  return (
+    
+      <div>
+  
+      <ResponsiveAppBar />
+    <MediaCover />
+    <  ColorInversionFooter />
+    </div>
+  )
+}
+
+export default App
